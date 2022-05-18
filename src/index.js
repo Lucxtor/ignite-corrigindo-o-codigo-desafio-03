c6587e8949f70c418e3ebfcdda072a09a9485352
Luis Felipe Fabiane <luisffabiane@gmail.com>
2022-05-18 01:31:52 -0300

    Corrigindo retorno
    Retornando o número de likes do repositório em substituição da string likes apenas

--- src/index.js
+++ src/index.js
@@ -70,7 +70,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json({likes: repositories[repositoryIndex].likes});
 });
 
 module.exports = app;
